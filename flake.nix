30c35cb7c00a473f1cf3c25e98940607e5ff03d1
xeruf <27jf@pm.me>
2023-09-25 17:25:47 +0200

    First Strike

---
+++ flake.nix
@@ -0,0 +1,36 @@
+{
+    inputs = {
+        ihp.url = "github:digitallyinduced/ihp/v1.1";
+        nixpkgs.follows = "ihp/nixpkgs";
+        flake-parts.follows = "ihp/flake-parts";
+        devenv.follows = "ihp/devenv";
+        systems.follows = "ihp/systems";
+    };
+
+    outputs = inputs@{ ihp, flake-parts, systems, ... }:
+        flake-parts.lib.mkFlake { inherit inputs; } {
+
+            systems = import systems;
+            imports = [ ihp.flakeModules.default ];
+
+            perSystem = { pkgs, ... }: {
+                ihp = {
+                    enable = true;
+                    projectPath = ./.;
+                    packages = with pkgs; [
+                        # Native dependencies, e.g. imagemagick
+                    ];
+                    haskellPackages = p: with p; [
+                        # Haskell dependencies go here
+                        p.ihp
+                        cabal-install
+                        base
+                        wai
+                        text
+                        hlint
+                    ];
+                };
+            };
+
+        };
+}
